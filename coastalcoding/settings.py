5d3361658e25f6cc3319563ceda451b57e8363ca
jbrennan094 <jbrennan094@gmail.com>
2019-06-01 09:02:26 -0700

    Updated for heroku deployment

--- coastalcoding/settings.py
+++ coastalcoding/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'd+7()zsu81zp6^h6!mul*04_^#_+zmw+b#y1nd4t_q9*d&a2r6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coastalcoding.herokuapp.com']
 
 
 # Application definition
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'coastalcoding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
