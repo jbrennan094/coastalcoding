206906c202a2291ef9434b707c6f5503875ddbb7
jbrennan094 <jbrennan094@gmail.com>
2019-06-01 05:40:01 -0700

    Adding the backend

---
+++ backend/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jbren\desktop\coasta~2\website\backend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
